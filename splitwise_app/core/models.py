75712ca6ab33bbbfb072266e14f6bab674035a43
a.d.devnandan <a.d.devnandan@gmail.com>
2025-12-19 20:44:43 +0530

    user model

---
+++ splitwise_app/core/models.py
@@ -0,0 +1,44 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, name, email=None, password=None):
+        if email:
+            email = self.normalize_email(email)
+
+        user = self.model(
+            id=uuid.uuid4(),
+            name=name,
+            email=email,
+        )
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+        return self.name
